c506c74738ed92d48c9ed64cade94f9f1e3d8a11
Tomoki Sato <tomokirenrakuyo@gmail.com>
2025-09-02 12:28:33 +0900

    Initial Commit

---
+++ NowPlayingOnMenubar/NowPlayingOnMenubarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowPlayingOnMenubarApp.swift
+//  NowPlayingOnMenubar
+//
+//  Created by tomoki sato on 2025/09/02.
+//
+
+import SwiftUI
+
+@main
+struct NowPlayingOnMenubarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NowPlayingOnMenubarTests/NowPlayingOnMenubarTests.swift
@@ -0,0 +1,17 @@
+//
+//  NowPlayingOnMenubarTests.swift
+//  NowPlayingOnMenubarTests
+//
+//  Created by tomoki sato on 2025/09/02.
+//
+
+import Testing
+@testable import NowPlayingOnMenubar
+
+struct NowPlayingOnMenubarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
